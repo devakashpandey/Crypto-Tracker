cb8dc6753a5ad43a59d996235e8b1e437a397c74
DevAkky <developerakky@gmaiil.com>
2023-02-19 14:42:30 +0530

    chart loder

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import "./CoinChart.css";
+import { HistoricalChart } from '../config/api';
+import { UseCryptoValue } from "../context/CryptoContext";
+import { CircularProgress, createTheme, ThemeProvider } from '@mui/material';
+import { Line } from "react-chartjs-2"
 
-const CoinChart = () => {
+const CoinChart = ({coin}) => {
+
+ const [chartData, setChartData] = useState();
+ const [days, setDays] = useState(1);
+
+ const { currency } =  UseCryptoValue();
+
+ const fetchChartData = () => {
+    const { data } = axios.get(HistoricalChart(coin.id, days, currency))
+    
+    setChartData(data?.prices)
+}
+
+useEffect(() => {
+   fetchChartData()
+}, [currency, days])
+
+
+  // --- this for the dark mode ----
+  const darkTheme = createTheme({
+     palette: {
+     mode: 'dark',
+    },
+});
+
+
   return (
       <>
-      COIN CHART
+      <ThemeProvider theme={darkTheme}>
+        <div className='chart-container'>
+
+          {
+            !chartData ? ( 
+               <CircularProgress thickness={2} size={200}/>
+            ) : (
+              <>
+              <Line />
+              
+              </>
+            )
+          }
+            
+        </div>
+       </ThemeProvider>
       </>
   )
 }
