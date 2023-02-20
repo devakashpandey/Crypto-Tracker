3c4b5d28b2d1876abc6a64cff2f6d80bf1e89e03
DevAkky <developerakky@gmaiil.com>
2023-02-20 22:29:46 +0530

    coin graph done

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import ContextProvider from './context/CryptoContext';
 import 'react-alice-carousel/lib/alice-carousel.css';  // css of alice caresoul
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+
   <BrowserRouter>
     <React.StrictMode>
+
       <ContextProvider>
         <App />
       </ContextProvider>
+
     </React.StrictMode>
   </BrowserRouter>
 );
